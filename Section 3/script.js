cb2238026a2f7f0526f42a6ad1413005ad4bfa4d
Dwayne Redwood <dwayneredwood@live.com>
2023-09-20 22:13:39 -0500

    coding challenge vid 62

--- Section 3/script.js
+++ Section 3/script.js
@@ -1,15 +1,29 @@
 'use strict';
-const measureKelvin = function () {
-    const measurement = {
-        type: 'temp',
-        unit: 'celcius',
-        value: Number(prompt('Degrees celcius;')),
-    };
+// const measureKelvin = function () {
+//     const measurement = {
+//         type: 'temp',
+//         unit: 'celcius',
+//         value: Number(prompt('Degrees celcius;')),
+//     };
 
-    debugger;
-    console.table(measurement);
-    const kelvin = measurement.value + 273;
-    return kelvin;
+//     debugger;
+//     console.table(measurement);
+//     const kelvin = measurement.value + 273;
+//     return kelvin;
+// }
+
+// console.log(measureKelvin());
+
+const data1 = [17, 21, 23]
+const data2 = [12, 5, -5, 0, 4]
+
+const printForecast = function(arr) {
+    let temp = ''
+    for (let i =0; i<arr.length; i++){
+        temp += `${arr[i]}°C in ${i+1} days ... `;
+    }
+    console.log('... ' + temp);
 }
 
-console.log(measureKelvin());
+printForecast(data1);
+printForecast(data2);
