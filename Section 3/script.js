dbc64e8fce1745d8cf7c61dc7b40f6f705c82f59
Dwayne Redwood <dwayneredwood@live.com>
2023-09-20 21:39:40 -0500

    lesson 61 competed

--- Section 3/script.js
+++ Section 3/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const measureKelvin = function () {
+    const measurement = {
+        type: 'temp',
+        unit: 'celcius',
+        value: Number(prompt('Degrees celcius;')),
+    };
+
+    debugger;
+    console.table(measurement);
+    const kelvin = measurement.value + 273;
+    return kelvin;
+}
+
+console.log(measureKelvin());
