6ef4cd441f731419fce881f5df4573b35def4e22
Dwayne Redwood <dwayneredwood@live.com>
2023-09-22 00:35:41 -0500

    Added comments

--- Section 5 - Guess My Number/script.js
+++ Section 5 - Guess My Number/script.js
@@ -8,11 +8,17 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-
+    
+    //When there is no input
     if(!guess){
         document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    //When player wins
     } else if(guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+    
+    
+        //When guess is too high
     } else if(guess > secretNumber){
         if(score >1) {
             document.querySelector('.message').textContent = '📈 Too High!';
@@ -22,6 +28,8 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.message').textContent = '💥You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+
+        //When guess is too low
     } else if(guess < secretNumber){
         if(score >1) {
             document.querySelector('.message').textContent = '📉 Too Low!';
