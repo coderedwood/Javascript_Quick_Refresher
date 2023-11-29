cbed8bf32e12d85335ecdc7d3af6a944fffd66e2
Dwayne Redwood <dwayneredwood@live.com>
2023-11-28 23:21:07 -0500

    scoping practice, watched the rest and digested

--- Section 8/script.js
+++ Section 8/script.js
@@ -1,1 +1,28 @@
-'use strict';
+// 'use strict';
+
+function calAge(birthYear){
+    const age = 2037- birthYear;
+  function printAge(){
+    const output = `${firstName}, You are ${age} born in ${birthYear}`;
+    console.log(output);
+    if(birthYear>= 1981 && birthYear<=1996){
+        var millenial = true
+        const str = `Oh and you're a millenial, ${firstName}`;
+        console.log(str);
+
+        function add(a,b){
+            return a + b;
+        }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2,3));
+}
+  printAge();
+  return age;
+}
+
+const firstName = 'Dwayne';
+calAge(1991);
+// console.log(age);
+// printAge();
